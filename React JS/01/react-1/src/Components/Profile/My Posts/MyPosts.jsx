28b760d4aa5cd5fa5f434b2858a48599b9c8e922
chepLove <ilya.chepelov@gmail.com>
2021-09-28 20:14:46 +0300

    39 lesson

--- React JS/01/react-1/src/Components/Profile/My Posts/MyPosts.jsx
+++ React JS/01/react-1/src/Components/Profile/My Posts/MyPosts.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import style from './MyPosts.module.css'
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../Redux/State";
+
 
 const MyPosts = (props) => {
     let postsElements = props.posts
@@ -9,12 +11,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch({type: 'ADD-POST'})
+        props.dispatch(addPostActionCreator())
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value
-        props.dispatch({type: 'UPDATE-NEW-POST-TEXT',newText:text})
+        props.dispatch(updateNewPostTextActionCreator(text))
     }
     return (
         <div className={style.postsBlock}>
