f765ca6e26b01058b5a56d315213974738498474
chepLove <ilya.chepelov@gmail.com>
2021-11-20 15:38:16 +0300

    68

--- React JS/01/react-1/src/App.js
+++ React JS/01/react-1/src/App.js
@@ -9,6 +9,7 @@ import DialogsContainer from "./Components/Dialogs/DialogsContainer";
 import UsersContainer from "./Components/Users/UsersContainer";
 import ProfileContainer from "./Components/Profile/ProfileContainer";
 import HeaderContainer from "./Components/Header/HeaderContainer";
+import Login from "./Components/Login/Login";
 
 
 const App = (props) => {
@@ -23,6 +24,7 @@ const App = (props) => {
                 <Route path='/music' render={() => <Music/>}/>
                 <Route path='/news' render={() => <News/>}/>
                 <Route path='/settings' render={() => <Settings/>}/>
+                <Route path='/login' render={() => <Login/>}/>
             </div>
         </div>
     );
--- React JS/01/react-1/src/Components/Header/HeaderContainer.jsx
+++ React JS/01/react-1/src/Components/Header/HeaderContainer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./Header";
 import {connect} from "react-redux";
-import { getAuthUserData,} from "../../Redux/auth-reducer";
+import {getAuthUserData} from "../../Redux/auth-reducer";
 
 class HeaderContainer extends React.Component {
     componentDidMount() {
@@ -14,7 +14,7 @@ class HeaderContainer extends React.Component {
 }
 
 const mapStateToProps = (state) => ({
-    isAuth: state.auth.isAuth,
-    login:state.auth.login
+    isAuth: state.auth.state.isAuth,
+    login:state.auth.state.login
 })
 export default connect(mapStateToProps, {getAuthUserData})(HeaderContainer)
