370de86b8b2f2a2a546706e360a70d0af13a3c06
chepLove <ilya.chepelov@gmail.com>
2021-12-24 15:38:04 +0300

    87 lesson

--- React JS/01/react-1/src/Components/Profile/My Posts/MyPosts.jsx
+++ React JS/01/react-1/src/Components/Profile/My Posts/MyPosts.jsx
@@ -5,25 +5,25 @@ import {Field, reduxForm} from "redux-form";
 import {maxLengthCreator, required} from "../../../utils/validators/validators";
 import {Textarea} from "../../Common/FormsControls/FormsControls";
 
-const MyPosts = (props) => {
+const MyPosts = React.memo(props => {
     let postsElements = props.posts
         .map(p => <Post text={p.textPost} likesCount={p.likesCount}/>)
 
-    let addNewPost = (values)=>{
-         props.addPost(values.newPostText)
+    let addNewPost = (values) => {
+        props.addPost(values.newPostText)
     }
 
 
     return (
         <div className={style.postsBlock}>
             <h3>my posts</h3>
-            <AddPostFormRedux onSubmit = {addNewPost}/>
+            <AddPostFormRedux onSubmit={addNewPost}/>
             <div className={style.posts}>
                 {postsElements}
             </div>
         </div>
     )
-}
+});
 
 const maxLength = maxLengthCreator(10)
 
